32e61c5bc4861fa44ddb0403e26f9c2587d2ca81
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-04-08 22:50:27 +0700

    conplete redux type script

--- src/state/hooks/useTypedSelector.ts
+++ src/state/hooks/useTypedSelector.ts
@@ -1,4 +1,4 @@
 import  {useSelector , TypedUseSelectorHook} from 'react-redux'
-import {RootState} from '../state/reducers/rootReducer'
+import {RootState} from '../reducers/rootReducer'
 
 export const useTypedSelector:TypedUseSelectorHook<RootState> = useSelector
