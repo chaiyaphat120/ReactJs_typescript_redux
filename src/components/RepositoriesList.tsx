33359cf8d2dded38612cc81036a1973d9cf6b338
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2021-04-08 22:00:13 +0700

    add1

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,54 +1,31 @@
-import { useState } from "react"
 import { useTypedSelector } from "../state"
 import { useDispatch } from "react-redux"
-import { actionCreators } from "../state"
 import { ActionType } from "../state/types/action-types"
 const RepositoriesList: React.FC = () => {
-    const [term, setTerm] = useState("")
     const dispatch = useDispatch()
-    const { data, error, loading } = useTypedSelector(
-        (state) => state.repositoriesState
-    )
     const { moviesList } = useTypedSelector((state) => state.MoviesState)
 
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const onSubmit = (event: React.FormEvent<HTMLButtonElement>) => {
         event.preventDefault()
-        dispatch(actionCreators.searchRepositories(term))
+        dispatch({
+            type: ActionType.FETCH_DATA,
+            payload: { name: "art", price: 200, quantity: "200" },
+        })
     }
-
-    let DataComponent: React.ReactNode = <div>..</div>
-    const dataComponentFc = () => {
-        if (error && loading) {
-            return
-        }
-        return data.map((name) => <div key={name}>{name}</div>)
+    let todoComponent: React.ReactNode = <div></div>
+    if (moviesList.length) {
+        todoComponent = moviesList.map((movie) => {
+            return (
+                <div>
+                    {movie.name} : {movie.price} {movie.quantity}{" "}
+                </div>
+            )
+        })
     }
-    DataComponent = dataComponentFc()
-
     return (
         <div>
-            <button
-                onClick={() =>
-                    dispatch({
-                        type: ActionType.FETCH_DATA,
-                        payload: { name: "art", price: 200, quantity: "200" },
-                    })
-                }
-            >
-                Click
-            </button>
-            {moviesList.map(movie =>{
-                return(
-                    <div>{movie.name} : {movie.price} {movie.quantity}  </div>
-                )
-            })}
-            <form onSubmit={onSubmit}>
-                <input value={term} onChange={(e) => setTerm(e.target.value)} />
-                <button>Search</button>
-            </form>
-            {error && <h3>{error}</h3>}
-            {loading && <h3>loading...</h3>}
-            {DataComponent}
+            <button onClick={onSubmit}>Click</button>
+            {todoComponent}
         </div>
     )
 }
